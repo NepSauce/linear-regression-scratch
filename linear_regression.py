a279a34bfa86404777310672d85cd3fc32c20af7
NepSauce <greasyandmoist@gmail.com>
2026-03-05 23:03:26 -0400

    Refactor: Update class to reduce duplicated code and redudant error handling

--- linear_regression.py
+++ linear_regression.py
@@ -3,42 +3,36 @@ import csv
 class LinearRegressionScratch:
     column_count = 0
 
-    def __init__(self, learning_rate, __lambda):
+    def __init__(self, learning_rate, w, b, __lambda):
         self.learning_rate = learning_rate
         self.__lambda = __lambda
+        self.x_train = []
+        self.y_train = []
+        self.w = w
+        self.b = b
 
         # Read CSV file
         with open('data.csv', 'r') as file:
             csv_reader = csv.reader(file, delimiter=',')
 
-            try:
-                header = next(csv_reader)
-                column_count = len(header)
+            header = next(csv_reader)
+            self.column_count = len(header)
 
-                if column_count > 0:
-                    row_count = 0
-                    row_count += sum(1 for _ in csv_reader)
-
-            except StopIteration as e:
-                # Handle empty file case
-                print(e)
-                pass
+            rows = list(csv_reader)
+            self.row_count = len(rows)
 
 
-            print(f"Column Count: {column_count}")
-            print(f"Row Count: {row_count}")
+            print(f"Column Count: {self.column_count}")
+            print(f"Row Count: {self.row_count}")
 
-            x_train = [[0 for _ in range(column_count - 2)] for _ in range(row_count - 1)]
-            y_train = [0 for _ in range(row_count - 1)]
-
-            for each_row in csv_reader:
-                x_train.append(each_row[:-1])
-                y_train.append(each_row[-1])
+            for each_row in rows:
+                self.x_train.append([float(value) for value in each_row[:-1]])
+                self.y_train.append(float(each_row[-1]))
                 
         
-        print(f"Column Count: {column_count}")
-        print(f"Row Count: {row_count}")
+        print(f"Column Count: {self.column_count}")
+        print(f"Row Count: {self.row_count}")
 
-        x_train = [[0 for _ in range(column_count)] for _ in range(row_count - 1)]
+        
 
         # Linear Combination fx_wb_i = np.dot(x, w) + b
