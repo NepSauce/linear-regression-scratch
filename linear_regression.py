784332f97cb6de3b909ff542b114e477c3827981
NepSauce <greasyandmoist@gmail.com>
2026-03-05 22:40:02 -0400

    Refactor: Move CSV reading logic into the constructor

--- linear_regression.py
+++ linear_regression.py
@@ -2,23 +2,6 @@ class LinearRegressionScratch:
     column_count = 0
     row_count = 0
 
-    # Read CSV file
-    with open('fake_housing_data.csv', 'r') as file:
-        csv_reader = csv.reader(file, delimiter=',')
-
-        try:
-            header = next(csv_reader)
-            column_count = len(header)
-
-            if column_count > 0:
-                row_count = 1
-                row_count += sum(1 for _ in csv_reader)
-
-        except StopIteration as e:
-            # Handle empty file case
-            print(e)
-            pass
-
     x_train = [[0 for _ in range(column_count)] for _ in range(row_count - 1)]
 
 
@@ -27,7 +10,28 @@ class LinearRegressionScratch:
         self.learning_rate = learning_rate
         self.__lambda = __lambda
 
-        # Linear Combination fx_wb_i = np.dot(x, w) + b
+        # Read CSV file
+        with open('fake_housing_data.csv', 'r') as file:
+            csv_reader = csv.reader(file, delimiter=',')
+
+            try:
+                header = next(csv_reader)
+                column_count = len(header)
+
+                if column_count > 0:
+                    row_count = 1
+                    row_count += sum(1 for _ in csv_reader)
+
+            except StopIteration as e:
+                # Handle empty file case
+                print(e)
+                pass
+        
+        print()(f"Column Count: {column_count}")
+        print()(f"Row Count: {row_count}")
+        x_train = [[0 for _ in range(column_count)] for _ in range(row_count - 1)]
+
+            # Linear Combination fx_wb_i = np.dot(x, w) + b
 
 
 
