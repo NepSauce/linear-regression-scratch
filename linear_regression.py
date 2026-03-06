2091c83ca0d205ba481f343eddb171a878cb608c
NepSauce <greasyandmoist@gmail.com>
2026-03-05 22:33:38 -0400

    Feat: Implement CSV reading for housing data

--- linear_regression.py
+++ linear_regression.py
@@ -1,16 +1,33 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
 class LinearRegressionScratch:
-    df = pd.read_csv("fake_housing_data.csv")
+    column_count = 0
+    row_count = 0
+
+    # Read CSV file
+    with open('fake_housing_data.csv', 'r') as file:
+        csv_reader = csv.reader(file, delimiter=',')
+
+        try:
+            header = next(csv_reader)
+            column_count = len(header)
+
+            if column_count > 0:
+                row_count = 1
+                row_count += sum(1 for _ in csv_reader)
+
+        except StopIteration:
+            # Handle empty file case
+            pass
+
+    x_train = [[0 for _ in range(column_count)] for _ in range(row_count - 1)]
+
 
     def __init__(self, df, learning_rate, __lambda):
         self.df = df
         self.learning_rate = learning_rate
         self.__lambda = __lambda
 
-        # Linear Combination f_x_wb_i = np.dot(x, w) + b
+        # Linear Combination fx_wb_i = np.dot(x, w) + b
+
 
 
 
