a747a6c8c55ed5fa839a4d0da752ad33f9db7de4
NepSauce <greasyandmoist@gmail.com>
2026-03-05 22:51:21 -0400

    Refactor: Move main execution logic to a separate entry point

---
+++ entry.py
@@ -0,0 +1,8 @@
+from linear_regression import LinearRegressionScratch
+
+
+if __name__ == "__main__":
+    learning_rate = 0.01
+    __lambda = 0.1
+
+    model = LinearRegressionScratch(learning_rate, __lambda)
--- linear_regression.py
+++ linear_regression.py
@@ -30,9 +30,3 @@ class LinearRegressionScratch:
         x_train = [[0 for _ in range(column_count)] for _ in range(row_count - 1)]
 
         # Linear Combination fx_wb_i = np.dot(x, w) + b
-
-if __name__ == "__main__":
-    learning_rate = 0.01
-    __lambda = 0.1
-
-    model = LinearRegressionScratch(learning_rate, __lambda)
