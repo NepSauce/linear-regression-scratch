72e08f67d69ecc327ab7e6022deeb75e023c035f
NepSauce <greasyandmoist@gmail.com>
2026-03-08 15:00:16 -0300

    Feat: Finish prediction_accuracy

--- prediction_accuracy.py
+++ prediction_accuracy.py
@@ -1,18 +1,37 @@
-import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
-import matplotlib.pyplot as plt
 
 class PredictionAccuracy:
-    def __init__(self, learning_rate, w, b, lambda_):
+    def __init__(self, learning_rate):
         data = pd.read_csv('data.csv')
         X = data[['size_sqft', 'bedrooms', 'age']].values
         y = data['price'].values
         self.X = torch.tensor(X, dtype=torch.float32)
         self.y = torch.tensor(y, dtype=torch.float32).view(-1, 1)
+        # store model
+        self.model = nn.Linear(3, 1)
+        # loss function
+        self.criterion = nn.MSELoss()
+        # optimizer
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=learning_rate)
 
-        model = nn.Linear(3, 1)
-        criterion = nn.MSELoss()
 
-        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    def train(self, epochs=1000):
+        for epoch in range(epochs):
+            self.model.train()
+            self.optimizer.zero_grad()
+            predictions = self.model(self.X)
+            loss = self.criterion(predictions, self.y)
+            loss.backward()
+            self.optimizer.step()
+
+            if epoch % 100 == 0:
+                print(f'Epoch {epoch}/{epochs}, Loss: {loss.item()}')
+
+
+    def compute_prediction_accuracy(self):
+        new_house = torch.tensor([[2500, 4, 5]], dtype=torch.float32)
+        prediction = self.model(new_house)
+
+        print(f'Predicted price: {prediction.item()}')
